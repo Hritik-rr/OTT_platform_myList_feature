22eebb5339df0514c282292a8ca5932d2897d882
Hritik--rr <hritikranjan17@gmail.com>
2024-05-22 18:24:08 +0530

    Integration testing complete

--- dist/tests/integration/userRoutes.test.js
+++ dist/tests/integration/userRoutes.test.js
@@ -23,6 +23,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use('/', mainRoute_1.default);
 describe('User Routes', () => {
+    jest.setTimeout(30000);
     let mongoServer;
     beforeAll(() => __awaiter(void 0, void 0, void 0, function* () {
         mongoServer = yield mongodb_memory_server_1.MongoMemoryServer.create();
@@ -37,7 +38,7 @@ describe('User Routes', () => {
     beforeEach(() => __awaiter(void 0, void 0, void 0, function* () {
         yield User_1.default.deleteMany({});
     }));
-    it('Should create a new User data via POST /addNewUser', () => __awaiter(void 0, void 0, void 0, function* () {
+    it('Should create a new User data via POST /users/addNewUser', () => __awaiter(void 0, void 0, void 0, function* () {
         const response = yield (0, supertest_1.default)(app)
             .post('/users/addNewUser')
             .send({
@@ -102,5 +103,210 @@ describe('User Routes', () => {
                 watchedOn: "2024-01-16"
             }
         ]);
+    }));
+    it('Should retrieve user myList via GET /users/myList/:userId', () => __awaiter(void 0, void 0, void 0, function* () {
+        // Step 1: Add a movie to the collection
+        const movieResponse = yield (0, supertest_1.default)(app)
+            .post('/movies')
+            .send({
+            title: "Twilight",
+            description: "A man tries to find the truth about human exitence.",
+            genres: ["Action", "SciFi"],
+            releaseDate: "2016-07-16",
+            director: "Christopher Nolan",
+            actors: ["Keanu Reeves", "Ellen Page"]
+        });
+        expect(movieResponse.status).toBe(201);
+        const movie = movieResponse.body.data;
+        // Step 2: Add a TV show to the collection
+        const tvShowResponse = yield (0, supertest_1.default)(app)
+            .post('/tvShows')
+            .send({
+            title: "How I Met your Mother",
+            description: "This is an example TV show description.",
+            genres: ["Drama", "Fantasy"],
+            episodes: [
+                {
+                    episodeNumber: 1,
+                    seasonNumber: 1,
+                    releaseDate: "2024-01-01",
+                    director: "Jane Doe",
+                    actors: ["John Smith", "Mary Johnson"]
+                },
+                {
+                    episodeNumber: 2,
+                    seasonNumber: 1,
+                    releaseDate: "2024-01-08",
+                    director: "John Doe",
+                    actors: ["John Smith", "Mary Johnson"]
+                }
+            ]
+        });
+        expect(tvShowResponse.status).toBe(201);
+        const tvShow = tvShowResponse.body.data;
+        // Step 3: Add a user with the movie and TV show in their myList
+        const userResponse = yield (0, supertest_1.default)(app)
+            .post('/users/addNewUser')
+            .send({
+            username: "John Doe",
+            preferences: {
+                favoriteGenres: ["Action", "SciFi"],
+                dislikedGenres: ["Romance", "Comedy"]
+            },
+            watchHistory: [],
+            myList: [
+                { itemId: movie._id, itemType: "Movie" },
+                { itemId: tvShow._id, itemType: "TVShow" }
+            ]
+        });
+        expect(userResponse.status).toBe(201);
+        const user = userResponse.body.data;
+        // Step 4: Retrieve the user's myList
+        const response = yield (0, supertest_1.default)(app).get(`/users/myList/${user.userId}`);
+        // Step 5: Validate the response
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("User's myList retrieved successfully.");
+        expect(response.body.data).toEqual([
+            {
+                _id: movie._id,
+                title: "Twilight",
+                description: "A man tries to find the truth about human exitence.",
+                genres: ["Action", "SciFi"],
+                releaseDate: "2016-07-16T00:00:00.000Z",
+                director: "Christopher Nolan",
+                actors: ["Keanu Reeves", "Ellen Page"],
+                __v: 0
+            },
+            {
+                _id: tvShow._id,
+                title: "How I Met your Mother",
+                description: "This is an example TV show description.",
+                genres: ["Drama", "Fantasy"],
+                episodes: [
+                    {
+                        episodeNumber: 1,
+                        seasonNumber: 1,
+                        releaseDate: "2024-01-01T00:00:00.000Z",
+                        director: "Jane Doe",
+                        actors: ["John Smith", "Mary Johnson"],
+                        _id: tvShow.episodes[0]._id
+                    },
+                    {
+                        episodeNumber: 2,
+                        seasonNumber: 1,
+                        releaseDate: "2024-01-08T00:00:00.000Z",
+                        director: "John Doe",
+                        actors: ["John Smith", "Mary Johnson"],
+                        _id: tvShow.episodes[1]._id
+                    }
+                ],
+                __v: 0
+            }
+        ]);
+        expect(response.body.pagination).toEqual({
+            currentPage: 1,
+            totalPages: 1,
+            totalItems: 2
+        });
+    }));
+    it('Should modify user\'s myList by adding values via PATCH /users/modifyMyList/:userId', () => __awaiter(void 0, void 0, void 0, function* () {
+        // Prepare the user's data or fetch it from the database
+        const user = {
+            userId: 'd313eea9-b327-46c1-a9c6-057464e781de',
+            username: 'Test User',
+            preferences: {
+                favoriteGenres: ['Action', 'SciFi'],
+                dislikedGenres: ['Romance', 'Comedy']
+            },
+            watchHistory: [
+                {
+                    contentId: '664d568921972e80848ecb78',
+                    watchedOn: '2024-01-15',
+                    rating: 4
+                },
+                {
+                    contentId: '664c9c37f04d0f8b0a64e492',
+                    watchedOn: '2024-01-16'
+                }
+            ],
+            myList: [
+                {
+                    itemId: '664c9c37f04d0f8b0a64e492',
+                    itemType: 'Movie'
+                },
+                {
+                    itemId: '664d5a5265a9bfb557bc6e35',
+                    itemType: 'TVShow'
+                }
+            ]
+        };
+        // Insert the user data into the database or update it if already exists
+        yield User_1.default.findOneAndUpdate({ userId: user.userId }, user, { upsert: true });
+        // Prepare the request body
+        const requestBody = {
+            itemId: '664dd3e2d3023d5c869bd2b1',
+            itemType: 'TVShow'
+        };
+        // Send PATCH request to modify user's myList
+        const response = yield (0, supertest_1.default)(app)
+            .patch(`/users/modifyMyList/${user.userId}`)
+            .send(requestBody);
+        // Assertions
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("Item added to user's myList successfully.");
+        expect(response.body.data).toHaveLength(3); // Assuming 3 items in the user's myList after modification
+        // You may add more specific assertions based on your requirements
+    }));
+    it('Should modify user\'s myList by deleting values via PATCH /users/modifyMyList/:userId', () => __awaiter(void 0, void 0, void 0, function* () {
+        // Prepare the user's data or fetch it from the database
+        const user = {
+            userId: 'd313eea9-b327-46c1-a9c6-057464e781de',
+            username: 'Test User',
+            preferences: {
+                favoriteGenres: ['Action', 'SciFi'],
+                dislikedGenres: ['Romance', 'Comedy']
+            },
+            watchHistory: [
+                {
+                    contentId: '664d568921972e80848ecb78',
+                    watchedOn: '2024-01-15',
+                    rating: 4
+                },
+                {
+                    contentId: '664c9c37f04d0f8b0a64e492',
+                    watchedOn: '2024-01-16'
+                }
+            ],
+            myList: [
+                {
+                    itemId: '664c9c37f04d0f8b0a64e492',
+                    itemType: 'Movie'
+                },
+                {
+                    itemId: '664d5a5265a9bfb557bc6e35',
+                    itemType: 'TVShow'
+                },
+                {
+                    itemId: '664dd3e2d3023d5c869bd2b1',
+                    itemType: 'TVShow'
+                }
+            ]
+        };
+        // Insert the user data into the database or update it if already exists
+        yield User_1.default.findOneAndUpdate({ userId: user.userId }, user, { upsert: true });
+        // Prepare the request body
+        const requestBody = {
+            itemId: '664dd3e2d3023d5c869bd2b1',
+            itemType: 'TVShow'
+        };
+        // Send DELETE request to remove item from user's myList
+        const response = yield (0, supertest_1.default)(app)
+            .delete(`/users/modifyMyList/${user.userId}`)
+            .send(requestBody);
+        // Assertions
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("Item removed from user's myList successfully. Remaining myList items below.");
+        expect(response.body.data).toHaveLength(2); // Assuming 2 items remain in the user's myList after removal
+        // You may add more specific assertions based on your requirements
     }));
 });
--- src/tests/integration/userRoutes.test.ts
+++ src/tests/integration/userRoutes.test.ts
@@ -11,6 +11,7 @@ app.use(express.json());
 app.use('/', routes);
 
 describe('User Routes', () => {
+    jest.setTimeout(30000);
     let mongoServer: MongoMemoryServer;
 
     beforeAll(async () => {
@@ -29,7 +30,8 @@ describe('User Routes', () => {
         await User.deleteMany({});
     });
 
-    it('Should create a new User data via POST /addNewUser', async () => {
+    it('Should create a new User data via POST /users/addNewUser', async () => {
+
         const response = await request(app)
         .post('/users/addNewUser')
         .send({
@@ -81,7 +83,7 @@ describe('User Routes', () => {
             }        
         ]);
 
-        // Format the watchedOn dates before comparing
+        // Formatting the watchedOn dates before comparing
         const formattedWatchHistory = response.body.data.watchHistory.map((entry: any) => ({
             ...entry,
             watchedOn: formatISO(new Date(entry.watchedOn), { representation: 'date' })
@@ -101,4 +103,222 @@ describe('User Routes', () => {
             }
         ]);
     });
+
+    it('Should retrieve user myList via GET /users/myList/:userId', async () => {
+        // Step 1: Adding a movie to the collection
+        const movieResponse = await request(app)
+            .post('/movies')
+            .send({
+                title: "Twilight",
+                description: "A man tries to find the truth about human exitence.",
+                genres: ["Action", "SciFi"],
+                releaseDate: "2016-07-16",
+                director: "Christopher Nolan",
+                actors: ["Keanu Reeves", "Ellen Page"]
+            });
+
+        expect(movieResponse.status).toBe(201);
+        const movie = movieResponse.body.data;
+
+        // Step 2: Adding a TV show to the collection
+        const tvShowResponse = await request(app)
+            .post('/tvShows')
+            .send({
+                title: "How I Met your Mother",
+                description: "This is an example TV show description.",
+                genres: ["Drama", "Fantasy"],
+                episodes: [
+                    {
+                        episodeNumber: 1,
+                        seasonNumber: 1,
+                        releaseDate: "2024-01-01",
+                        director: "Jane Doe",
+                        actors: ["John Smith", "Mary Johnson"]
+                    },
+                    {
+                        episodeNumber: 2,
+                        seasonNumber: 1,
+                        releaseDate: "2024-01-08",
+                        director: "John Doe",
+                        actors: ["John Smith", "Mary Johnson"]
+                    }
+                ]
+            });
+
+        expect(tvShowResponse.status).toBe(201);
+        const tvShow = tvShowResponse.body.data;
+
+        // Step 3: Add a user with the movie and TV show in their myList
+        const userResponse = await request(app)
+            .post('/users/addNewUser')
+            .send({
+                username: "John Doe",
+                preferences: {
+                    favoriteGenres: ["Action", "SciFi"],
+                    dislikedGenres: ["Romance", "Comedy"]
+                },
+                watchHistory: [],
+                myList: [
+                    { itemId: movie._id, itemType: "Movie" },
+                    { itemId: tvShow._id, itemType: "TVShow" }
+                ]
+            });
+
+        expect(userResponse.status).toBe(201);
+        const user = userResponse.body.data;
+
+        // Step 4: Retrieve the user's myList
+        const response = await request(app).get(`/users/myList/${user.userId}`);
+
+        // Step 5: Validate the response
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("User's myList retrieved successfully.");
+        expect(response.body.data).toEqual([
+            {
+                _id: movie._id,
+                title: "Twilight",
+                description: "A man tries to find the truth about human exitence.",
+                genres: ["Action", "SciFi"],
+                releaseDate: "2016-07-16T00:00:00.000Z",
+                director: "Christopher Nolan",
+                actors: ["Keanu Reeves", "Ellen Page"],
+                __v: 0
+            },
+            {
+                _id: tvShow._id,
+                title: "How I Met your Mother",
+                description: "This is an example TV show description.",
+                genres: ["Drama", "Fantasy"],
+                episodes: [
+                    {
+                        episodeNumber: 1,
+                        seasonNumber: 1,
+                        releaseDate: "2024-01-01T00:00:00.000Z",
+                        director: "Jane Doe",
+                        actors: ["John Smith", "Mary Johnson"],
+                        _id: tvShow.episodes[0]._id
+                    },
+                    {
+                        episodeNumber: 2,
+                        seasonNumber: 1,
+                        releaseDate: "2024-01-08T00:00:00.000Z",
+                        director: "John Doe",
+                        actors: ["John Smith", "Mary Johnson"],
+                        _id: tvShow.episodes[1]._id
+                    }
+                ],
+                __v: 0
+            }
+        ]);
+        expect(response.body.pagination).toEqual({
+            currentPage: 1,
+            totalPages: 1,
+            totalItems: 2
+        });
+    });    
+
+    it('Should modify user\'s myList by adding values via PATCH /users/modifyMyList/:userId', async () => {
+        const user = {
+            userId: 'd313eea9-b327-46c1-a9c6-057464e781de',
+            username: 'Test User',
+            preferences: {
+                favoriteGenres: ['Action', 'SciFi'],
+                dislikedGenres: ['Romance', 'Comedy']
+            },
+            watchHistory: [
+                {
+                    contentId: '664d568921972e80848ecb78',
+                    watchedOn: '2024-01-15',
+                    rating: 4
+                },
+                {
+                    contentId: '664c9c37f04d0f8b0a64e492',
+                    watchedOn: '2024-01-16'
+                }
+            ],
+            myList: [
+                {
+                    itemId: '664c9c37f04d0f8b0a64e492',
+                    itemType: 'Movie'
+                },
+                {
+                    itemId: '664d5a5265a9bfb557bc6e35',
+                    itemType: 'TVShow'
+                }
+            ]
+        };
+
+        // Finding the data on the basis of userId
+        await User.findOneAndUpdate({ userId: user.userId }, user, { upsert: true });
+
+        // JSON request body
+        const requestBody = {
+            itemId: '664dd3e2d3023d5c869bd2b1',
+            itemType: 'TVShow'
+        };
+
+        // Making PATCH request to modify user's myList
+        const response = await request(app)
+            .patch(`/users/modifyMyList/${user.userId}`)
+            .send(requestBody);
+
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("Item added to user's myList successfully.");
+        expect(response.body.data).toHaveLength(3); 
+    });    
+
+    it('Should modify user\'s myList by deleting values via PATCH /users/modifyMyList/:userId', async () => {
+        const user = {
+            userId: 'd313eea9-b327-46c1-a9c6-057464e781de',
+            username: 'Test User',
+            preferences: {
+                favoriteGenres: ['Action', 'SciFi'],
+                dislikedGenres: ['Romance', 'Comedy']
+            },
+            watchHistory: [
+                {
+                    contentId: '664d568921972e80848ecb78',
+                    watchedOn: '2024-01-15',
+                    rating: 4
+                },
+                {
+                    contentId: '664c9c37f04d0f8b0a64e492',
+                    watchedOn: '2024-01-16'
+                }
+            ],
+            myList: [
+                {
+                    itemId: '664c9c37f04d0f8b0a64e492',
+                    itemType: 'Movie'
+                },
+                {
+                    itemId: '664d5a5265a9bfb557bc6e35',
+                    itemType: 'TVShow'
+                },
+                {
+                    itemId: '664dd3e2d3023d5c869bd2b1',
+                    itemType: 'TVShow'
+                }
+            ]
+        };
+    
+        // Finding the data on the basis of userId
+        await User.findOneAndUpdate({ userId: user.userId }, user, { upsert: true });
+    
+        // JSON request body
+        const requestBody = {
+            itemId: '664dd3e2d3023d5c869bd2b1',
+            itemType: 'TVShow'
+        };
+    
+        // Sending the DELETE request to remove item from user's myList
+        const response = await request(app)
+            .delete(`/users/modifyMyList/${user.userId}`)
+            .send(requestBody);
+    
+        // Assertions
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("Item removed from user's myList successfully. Remaining myList items below.");
+        expect(response.body.data).toHaveLength(2); 
+    });    
 });
